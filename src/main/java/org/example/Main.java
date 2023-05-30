4cbc2f41a88bb82549e6e2c207265fedb01a2f5d
ElmaRuis <elmatampubolon@gmail.com>
2023-05-30 20:17:56 +0800

    CREATE TABLE ECOMMERCE

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,27 +7,23 @@ package org.example;
     import java.io.IOException;
     import java.io.OutputStream;
     import java.net.InetSocketAddress;
-    import java.util.concurrent.Executor;
     import java.util.concurrent.Executors;
 public class Main {
-    public static void main (String[] args) throws IOException
-    {
-        public class Main {
-            public static void main (String[] args) throws IOException
-            {
+    public static void main(String[] args) throws IOException {
                 HttpServer httpServer = HttpServer.create(new InetSocketAddress("Localhost", 8074), 0);
 
                 httpServer.createContext("/ecommerce", new ecommerceHandler());
                 httpServer.setExecutor(Executors.newSingleThreadExecutor());
                 httpServer.start();
             }
+
             public static class ecommerceHandler implements HttpHandler {
                 @Override
                 public void handle(HttpExchange exchange) throws IOException {
                     //post
                     //get
 
-                    if("GET".equals(exchange.getRequestMethod())){
+                    if ("GET".equals(exchange.getRequestMethod())) {
                         OutputStream outputStream = exchange.getResponseBody();
                         String tes = "Elma";
                         exchange.sendResponseHeaders(200, tes.length());
@@ -38,5 +34,3 @@ public class Main {
                 }
             }
         }
-
-    }
